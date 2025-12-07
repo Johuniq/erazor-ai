999bc152401704d7df36e5e48f0a0e4453465b72
Shejan Mahamud <mdshejanmahamud006@gmail.com>
2025-12-07 14:50:37 +0600

    refactor: Improve Header and Footer rendering logic in RootClient component based on pathname

--- components/RootClient.tsx
+++ components/RootClient.tsx
@@ -1,18 +1,18 @@
-"use client"
+"use client";
 
-import { usePathname } from "next/navigation"
-import { Footer } from "./footer"
-import { Header } from "./header"
+import { usePathname } from "next/navigation";
+import { Footer } from "./footer";
+import { Header } from "./header";
 
 export function RootClient({ children }: { children: React.ReactNode }) {
-    const pathname = usePathname()
-  return <>
-  {
-    pathname != "/dashboard/*" && <Header />
-  }
-  {children}
-    {
-    pathname != "/dashboard/*" && <Footer />
-  }
-  </>
+  const pathname = usePathname();
+  const inDashboard = pathname.startsWith("/dashboard");
+
+  return (
+    <>
+      {!inDashboard && <Header />}
+      {children}
+      {!inDashboard && <Footer />}
+    </>
+  );
 }
